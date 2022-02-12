301c49b23b5000a731e757421a34c0438346e955
Jun Seung Hyun <jsh5408@naver.com>
2022-02-12 18:17:42 +0900

    Use Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+import {createStore} from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
+const countModifier = (count = 0, action) => {
+    console.log(count);
+    if(action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
+}
+const countStore = createStore(countModifier);
 
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+
+console.log(countStore.getState());
+
+
+
+/*
+let count = 0;
 number.innerText = count;
 
 const updateText = () => {
@@ -24,3 +48,4 @@ const handleMinus = () => {
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
+*/
