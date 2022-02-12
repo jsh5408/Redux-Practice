94c743947dc670df95ade4ec7b1e732593a9b916
Jun Seung Hyun <jsh5408@naver.com>
2022-02-12 18:32:55 +0900

    Apply Store and Reducer

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
-    console.log(count);
+    //console.log(count);
     if(action.type === "ADD") {
         return count + 1;
     } else if (action.type === "MINUS") {
@@ -16,13 +18,23 @@ const countModifier = (count = 0, action) => {
 }
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "MINUS"});
+const onChange = () => {
+    //console.log("change");
+    number.innerText = countStore(countModifier);
+}
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+    countStore.dispatch({type: "ADD"});
+}
+
+const handleMinus = () => {
+    countStore.dispatch({type: "MINUS"});
+}
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 
 
