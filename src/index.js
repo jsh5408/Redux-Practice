18872a29f8e5e33a1f99a79e7b4da136256429eb
Jun Seung Hyun <jsh5408@naver.com>
2022-02-13 11:05:16 +0900

    Create JS To Do List

--- src/index.js
+++ src/index.js
@@ -1,77 +1,18 @@
-import {createStore} from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
-    //console.log(count);
-    switch(action.type) {
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count - 1;
-        default:
-            return count;
-    }
-}
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-    //console.log("change");
-    number.innerText = countStore(countModifier);
+const createToDo = (toDo) => {
+    const li = document.createElement("li");
+    li.innerText = toDo;
+    ul.appendChild(li);
 }
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-    countStore.dispatch({type: "ADD"});
+const onSubmit = (e) => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    createToDo(toDo);
 }
 
-const handleMinus = () => {
-    countStore.dispatch({type: "MINUS"});
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
-
-
-/*
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-    number.innterText = count;
-}
-
-const handleAdd = () => {
-    console.log("add");
-    count = count + 1;
-    updateText();
-}
-
-const handleMinus = () => {
-    console.log("minus");
-    count = count - 1;
-    updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-*/
-
-/*
-if(action.type === "ADD") {
-        return count + 1;
-    } else if (action.type === "MINUS") {
-        return count - 1;
-    } else {
-        return count;
-    }
-*/
+form.addEventListener("submit", onSubmit);
