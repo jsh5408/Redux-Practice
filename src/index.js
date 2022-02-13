d63fb4f17d0821c29cc5e136864af2a010fa1414
Jun Seung Hyun <jsh5408@naver.com>
2022-02-13 10:55:17 +0900

    Fix Better Way
    Switch 이용
    constant 변수로 오류 방지

--- src/index.js
+++ src/index.js
@@ -6,14 +6,18 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
     //console.log(count);
-    if(action.type === "ADD") {
-        return count + 1;
-    } else if (action.type === "MINUS") {
-        return count - 1;
-    } else {
-        return count;
+    switch(action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
     }
 }
 const countStore = createStore(countModifier);
@@ -60,4 +64,14 @@ const handleMinus = () => {
 
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
+*/
+
+/*
+if(action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
 */
